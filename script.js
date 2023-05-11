c3a178b3e2bcab0e5efaddb8a35e1b8e2e9f31e4
leoarchi456 <118028532+leoarchi456@users.noreply.github.com>
2023-05-10 20:22:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,4 +14,28 @@ if(close) {
         nav.classList.remove('active');
     })
 }
+// Define el ID del producto seleccionado
+var productId = obtenerIdProductoSeleccionado();
+
+// Obtiene los datos del producto seleccionado a través de un API o una base de datos
+var productoSeleccionado = obtenerDatosProducto(productId);
+
+// Inserta la información del producto seleccionado en el HTML
+var detalleProducto = document.querySelector("#prodetails");
+
+detalleProducto.innerHTML = `
+  <h2>${productoSeleccionado.nombre}</h2>
+  <img src="${productoSeleccionado.imagen}">
+  <p>${productoSeleccionado.descripcion}</p>
+  <p>Precio: ${productoSeleccionado.precio}</p>
+  <button onclick="añadirAlCarrito(${productId})">Añadir al carrito</button>
+`;
+
+// Define una función para añadir el producto al carrito
+function añadirAlCarrito(idProducto) {
+  // Agrega el producto seleccionado al carrito (usando un API o una base de datos)
+  // ...
+  console.log(`Producto ${idProducto} añadido al carrito`);
+}
+
 
